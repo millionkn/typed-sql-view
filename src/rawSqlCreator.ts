9ff40a78ffc44ff18bba9906a4709604b188b005
millionkn <millionkn@sina.com>
2024-04-09 17:41:09 +0800

    aggrateView自动去掉order,避免一些问题

--- src/rawSqlCreator.ts
+++ src/rawSqlCreator.ts
@@ -10,7 +10,7 @@ export class RawSqlCreator {
     }
   ) { }
 
-  selectAll<VT extends { [key: string]: Column<boolean, {} | null> }>(view: SqlView<VT>) {
+  private rawSelectAll<VT extends { [key: string]: Column<boolean, {} | null> }>(view: SqlView<VT>, ctx: { order: boolean }) {
     const buildCtx = SqlView.createBuildCtx(view)
     const selectTemplate = buildCtx.template
     const mapper2 = new Map<InnerColumn, string>()
@@ -22,7 +22,7 @@ export class RawSqlCreator {
       formatCbArr.push((raw) => [key, withNull && raw[alias] === null ? null : format(raw[alias])])
     })
     const sqlBody = buildCtx.analysis({
-      order: true,
+      order: ctx.order,
       usedColumn: [...mapper2.keys()],
     })
     let tableAliasIndex = 0
@@ -53,6 +53,10 @@ export class RawSqlCreator {
     }
   }
 
+  selectAll<VT extends { [key: string]: Column<boolean, {} | null> }>(view: SqlView<VT>) {
+    return this.rawSelectAll(view, { order: true })
+  }
+
   aggrateView<VT1 extends SqlViewTemplate, VT2 extends { [key: string]: Column<boolean, {} | null> }>(
     view: SqlView<VT1>,
     getTemplate: (expr: (target: (ref: GetRefStr<VT1>) => string) => Column<boolean, unknown>) => VT2,
@@ -60,6 +64,6 @@ export class RawSqlCreator {
     return view
       .bracketIf(({ state }) => hasOneOf(state, ['groupBy', 'skip', 'take']))
       .mapTo((e, define) => getTemplate((getTarget) => define((refStr) => getTarget((ref) => refStr(ref(e)))).withNull(true)))
-      .pipe((view) => this.selectAll(view))
+      .pipe((view) => this.rawSelectAll(view, { order: false }))
   }
 }
