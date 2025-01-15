b6535f202430e16578df97a4a74ba75b27ef3ead
millionkn <millionkn@sina.com>
2025-01-15 12:13:14 +0800

    导出

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { createSqlView } from './createSqlView.js'
 export { SqlView } from './sqlView.js'
 export type { SelectResult } from './sqlView.js'
-export type { Adapter, SqlState } from './tools.js'
+export type { Adapter, SqlState, SqlViewTemplate } from './tools.js'
 export { Column, SqlBody, createColumn } from './tools.js'
 export { SqlExecutor } from './sqlExecutor.js'
 export { resolveTableDefine } from './resolveTableDefine.js'
